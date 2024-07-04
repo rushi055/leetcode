88a382b8ee62587b2c062f19609a6f6554d388fc
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-07-04 15:51:27 +0530

    Update odd-even-linked-list.cpp

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -8,6 +8,8 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+////Approach-1 (Time : O(n), Space : O(1))
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
@@ -32,3 +34,7 @@ public:
         return head;
     }
 };
+
+// Approach-1 (Time : O(2n), Space : O(n))
+// we can also do by using two while loop in first while loop we will make two pointer even and odd first we will traverse in odd nodes Time complexity O(n/2) , then we will traverse in even nodes Time complexity O(n/2)
+// add save data in array or vector and then in second while loop we put the values from array in the existing linklist Time complexity O(n);
