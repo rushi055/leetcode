f6a0bcf1a6f888710d9a6c3ef7f78cc5be7c9030
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-07-04 15:22:26 +0530

    Update minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -1,3 +1,6 @@
+
+//T.C : O(nlogn)
+//S.C : O(1)
 class Solution {
 public:
     int minDifference(vector<int>& nums) {
@@ -12,6 +15,32 @@ public:
         result = min(result, nums[n - 1] - nums[3]);
         result = min(result, nums[n - 3] - nums[1]);
         result = min(result, nums[n - 2] - nums[2]);
+
+        return result;
+    }
+};
+
+//(Using Partial Sorting)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int minDifference(vector<int>& nums) {
+        int n = nums.size();
+        int result = INT_MAX;
+        if(n <= 4)
+            return 0;
+
+        partial_sort(begin(nums), begin(nums) + 4, end(nums));
+
+        nth_element(begin(nums) + 4, begin(nums) + (n - 4), end(nums));
+       
+        sort(begin(nums) + (n - 4), end(nums)); //4th largest element will be correct
+
+        result = min(result, nums[n-4] - nums[0]);
+        result = min(result, nums[n-1] - nums[3]);
+        result = min(result, nums[n-3] - nums[1]);
+        result = min(result, nums[n-2] - nums[2]);
 
         return result;
     }
