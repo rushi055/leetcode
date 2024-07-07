c691ef125637911e3a70c05521d2c9c6af6e610a
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-07-07 13:00:41 +0530

    Update intersection-of-two-linked-lists.cpp

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -29,3 +29,36 @@ public:
         return NULL;
     }
 };
+
+
+
+class Solution {
+public:
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        if (headA == NULL || headB == NULL) {
+            return NULL;
+        }
+
+        ListNode* a = headA;
+        ListNode* b = headB;
+
+        while (a != b) {
+
+            a = a->next;
+            b = b->next;
+
+            if (a == b) {
+                return a;
+            }
+
+            if (a == NULL) {
+                a = headB;
+            }
+            if (b == NULL) {
+                b = headA;
+            }
+        }
+
+        return a;
+    }
+};
