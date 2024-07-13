661d063f636506a1f2a6edebc678a79ae2b98382
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-07-13 14:33:15 +0530

    Time: 142 ms (99.62%) | Memory: 195.2 MB (87.79%) - LeetSync

---
+++ 2846-robot-collisions/robot-collisions.cpp
@@ -0,0 +1,56 @@
+const auto init = []() {
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+    return nullptr;
+}();
+
+class Solution {
+public:
+    vector<int> survivedRobotsHealths(vector<int>& positions,
+                                      vector<int>& healths, string directions) {
+        int n = positions.size();
+        vector<int> indices(n);
+
+        iota(indices.begin(), indices.end(), 0);
+        // This will fill the array as -> 0, 1, 2, 3, 4, n-1
+
+        stack<int> st;
+
+        auto lambda = [&](int i, int j) { return positions[i] < positions[j]; };
+        // we use & in [] bcoz to access the postions vector in the function
+
+        sort(begin(indices), end(indices), lambda);
+
+        vector<int> result;
+        for (int currentIndex : indices) {
+            if (directions[currentIndex] == 'R') {
+                st.push(currentIndex);
+            } else {
+                while (!st.empty() && healths[currentIndex] > 0) {
+                    int topIndex = st.top();
+                    st.pop();
+
+                    if (healths[topIndex] > healths[currentIndex]) {
+                        healths[topIndex] -= 1;
+                        healths[currentIndex] = 0;
+                        st.push(topIndex);
+                    } else if (healths[topIndex] < healths[currentIndex]) {
+                        healths[currentIndex] -= 1;
+                        healths[topIndex] = 0;
+                    } else {
+                        healths[currentIndex] = 0;
+                        healths[topIndex] = 0;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+            if (healths[i] > 0) {
+                result.push_back(healths[i]);
+            }
+        }
+        return result;
+    }
+};
