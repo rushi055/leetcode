cd9f30aa3ce6a03de38644793097b81e1ac8cc25
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-07-14 21:20:31 +0530

    Update merge-two-sorted-lists.cpp

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -33,5 +33,19 @@ public:
         }
 
         return dummy.next;
+
+    //      ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    //     if (l1 == nullptr) return l2;
+    // if (l2 == nullptr) return l1;
+
+    // // Recursive case
+    // if (l1->val < l2->val) {
+    //     l1->next = mergeTwoLists(l1->next, l2);
+    //     return l1;
+    // } else {
+    //     l2->next = mergeTwoLists(l1, l2->next);
+    //     return l2;
+    // }
+    //      }
     }
 };
