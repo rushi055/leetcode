005eb7c334e2b08ba86ef3f1c21adc2cb8f2b3d2
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-07-18 00:24:23 +0530

    Update permutations.cpp

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -29,3 +29,37 @@ public:
         return result;
     }
 };
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Solution {
+public:
+    vector<vector<int>> result;
+    int n;
+    
+    void solve(int idx, vector<int>& nums) {
+
+        if(idx == n) {
+            result.push_back(nums);
+            return;
+        }
+        
+        for(int i = idx; i < n; i++) {
+            swap(nums[i], nums[idx]);
+            
+            solve(idx+1, nums);
+            
+            swap(nums[i], nums[idx]);
+        }
+        
+        
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        n = nums.size();
+        
+        solve(0, nums);
+        
+        return result;
+    }
+};
