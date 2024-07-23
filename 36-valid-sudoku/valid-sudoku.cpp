a8eb8d3315ee852a4fd67c4d07e0e361b7551bca
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-07-23 12:24:31 +0530

    Update valid-sudoku.cpp

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -50,3 +50,27 @@ public:
         return true;
     }
 };
+
+//Approch 2
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string> st;
+        
+        for(int i = 0; i<9; i++) {
+            for(int j = 0; j<9; j++) {
+                if(board[i][j] == '.') continue;
+                
+                string row = string(1, board[i][j]) + "_row_" + to_string(i);
+                string col = string(1, board[i][j]) + "_col_" + to_string(j);
+                string box = string(1, board[i][j]) + "_box_" + to_string(i/3) + "_" + to_string(j/3);
+                if(st.count(row) || st.count(col) || st.count(box)) return false;
+                st.insert(row);
+                st.insert(col);
+                st.insert(box);
+            }
+        }
+        
+        return true;
+    }
+};
