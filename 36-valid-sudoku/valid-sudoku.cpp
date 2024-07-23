2dd2a7749423f5e48f3f3859f9219c70d16b77ba
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-07-23 12:18:02 +0530

    Time: 21 ms (32.19%) | Memory: 25.1 MB (27.26%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool validSub(vector<vector<char>>& board, int sr, int er, int sc, int ec) {
+        unordered_set<char> st;
+        for(int row = sr; row<=er; row++) {
+            for(int col = sc; col <= ec; col++) {
+                char ch = board[row][col];
+                if(ch == '.') continue;
+                if(st.count(ch)) return false;
+                st.insert(ch);
+            }
+        }
+        return true;
+    }
+    
+    bool isValidSudoku(vector<vector<char>>& board) {
+        //validate rows
+        for(int row = 0; row<9; row++) {
+            unordered_set<char> str;
+            for(int col = 0; col <9; col++) {
+                char ch = board[row][col];
+                if(ch == '.') continue;
+                if(str.count(ch)) return false;
+                str.insert(ch);
+            }
+        }
+        
+        //validate columns
+        for(int col = 0; col<9; col++) {
+            unordered_set<char> stc;
+            for(int row = 0; row <9; row++) {
+                char ch = board[row][col];
+                if(ch == '.') continue;
+                if(stc.count(ch)) return false;
+                stc.insert(ch);
+            }
+        }
+        
+		//validate ech 3*3 box
+        // we are iterating in each 3*3 box manually and then validating box 
+        for(int sr = 0; sr < 9; sr+=3) {
+            int er = sr+2;
+            for(int sc = 0; sc < 9; sc+=3) {
+                int ec = sc+2;
+                if(!validSub(board, sr, er, sc, ec))
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+};
