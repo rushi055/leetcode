8ca7f02319190a89e064f82d8ded10046fd2c148
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-07-27 00:13:16 +0530

    Update next-greater-element-i.cpp

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -32,3 +32,27 @@ public:
         return result;
     }
 };
+
+---------------------------------------------------------------------------------------------------------------------------------------------|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums2.size();
+        unordered_map<int, int>m;
+        stack<int>st;
+        for(int i=n-1; i>=0; i--){
+            while(!st.empty() && st.top() <= nums2[i]){
+                st.pop();
+            }
+            if(st.empty()) m[nums2[i]] = -1;
+            else m[nums2[i]] = st.top();
+            st.push(nums2[i]);
+        }
+        vector<int>ans;
+        for(auto it: nums1){
+            ans.push_back(m[it]);
+        }
+        return ans;
+    }
+};
