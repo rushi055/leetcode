801f30653b6e378c16d9e356743bc13bfe9f2052
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-07-27 00:11:29 +0530

    Time: 8 ms (30.81%) | Memory: 10.9 MB (84.44%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        vector<int> nextGE(nums2.size(), -1);
+
+        for (int i = nums2.size() - 1; i >= 0; --i) {
+            int element = nums2[i];
+
+            // Remove all elements smaller than the current element
+            while (!st.empty() && st.top() <= element) {
+                st.pop();
+            }
+
+            // If the stack is not empty, the top element is the next greater element
+            if (!st.empty()) {
+                nextGE[i] = st.top();
+            }
+
+            st.push(element);
+        }
+
+        vector<int> result;
+        for (int num : nums1) {
+            auto it = find(nums2.begin(), nums2.end(), num);
+            if (it != nums2.end()) {
+                int index = distance(nums2.begin(),it);    //it - nums2.begin();
+                result.push_back(nextGE[index]);
+            }
+        }
+
+        return result;
+    }
+};
