88d3035882b100b55c8f1448a87dad5d3e8cb001
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-08-01 15:19:40 +0530

    Update longest-substring-without-repeating-characters.cpp

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -16,3 +16,23 @@ public:
         return maxL;
     }
 };
+
+---------\---------------------\------------------------------\----------------------------------\------------------\-----------------------------\---------------------\------------------------------\----------------------------------\------------------
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> mp;
+        int maxL = 0;
+        int start = 0;
+
+        for (int end = 0; end < s.length(); end++) {
+            if (mp.find(s[end]) != mp.end()) {
+                start = max(start, mp[s[end]] + 1);
+            }
+            mp[s[end]] = end;
+            maxL = max(maxL, end - start + 1);
+        }
+
+        return maxL;
+    }
+};
