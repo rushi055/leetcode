efc9435d7feb8ed39f4c8177ce0668917ddedd5e
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-08-18 13:04:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (41.97%) - LeetSync

--- 263-ugly-number/ugly-number.cpp
+++ 263-ugly-number/ugly-number.cpp
@@ -1,17 +1,29 @@
+// class Solution {
+// public:
+//     bool isUgly(int n) {
+//         if (n <= 0)
+//             return false;
+//         while (n % 2 == 0) {
+//             n /= 2;
+//         }
+//         while (n % 3 == 0) {
+//             n /= 3;
+//         }
+//         while (n % 5 == 0) {
+//             n /= 5;
+//         }
+//         return n == 1;
+//     }
+// };
+
 class Solution {
 public:
     bool isUgly(int n) {
-        if (n <= 0)
-            return false;
-        while (n % 2 == 0) {
-            n /= 2;
-        }
-        while (n % 3 == 0) {
-            n /= 3;
-        }
-        while (n % 5 == 0) {
-            n /= 5;
-        }
-        return n == 1;
+        if (n <= 0) return false;
+        if (n == 1) return true;
+        if (n % 2 == 0) return isUgly(n / 2);
+        if (n % 3 == 0) return isUgly(n / 3);
+        if (n % 5 == 0) return isUgly(n / 5);
+        return false; 
     }
 };
