581fc8953fab01d1544d1a05b580c79c5df2c551
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-08-26 12:36:15 +0530

    Time: 11 ms (78.54%) | Memory: 15.3 MB (49.47%) - LeetSync

--- 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.cpp
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.cpp
@@ -20,18 +20,31 @@ public:
 
 class Solution {
 public:
-    void solve(Node* root, vector<int> &ans) {
-        if (root == NULL) {
-            return;
-        }
-        for (Node* child : root->children) {
-            solve(child, ans);
-        }
-        ans.push_back(root->val);
-    }
+    // void solve(Node* root, vector<int> &ans) {
+    //     if (root == NULL) {
+    //         return;
+    //     }
+    //     for (Node* child : root->children) {
+    //         solve(child, ans);
+    //     }
+    //     ans.push_back(root->val);
+    // }
     vector<int> postorder(Node* root) {
         vector<int> ans;
-        solve(root, ans);
+        if (root == nullptr)
+            return ans;
+        stack<Node*> st;
+        st.push(root);
+        while (!st.empty()) {
+            Node* node = st.top();
+            st.pop();
+            ans.push_back(node->val);
+
+            for (Node* child : node->children) {
+                st.push(child);
+            }
+        }
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
