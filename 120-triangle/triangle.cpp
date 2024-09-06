b4ca0041f5e2ebd6f976012d7d7fd692863e62ef
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-09-07 00:32:39 +0530

    Time: 8 ms (28.01%) | Memory: 11.8 MB (8.24%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,21 +1,47 @@
+// class Solution {
+// public:
+//     int minimumTotal(vector<vector<int>>& triangle){
+//         int n=triangle.size();
+//         vector<vector<int>> dp(n,vector<int>(n,0));
+//         for(int i=n-1;i>=0;i--)
+//         {
+//             for(int j=0;j<i+1;j++)
+//             {
+//                 if(i==n-1)
+//                 {
+//                     dp[i][j]=triangle[i][j];
+//                 }else
+//                 {
+//                     dp[i][j]=triangle[i][j]+min(dp[i+1][j],dp[i+1][j+1]);
+//                 }
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// };
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& triangle){
-        int n=triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,0));
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int j=0;j<i+1;j++)
-            {
-                if(i==n-1)
-                {
-                    dp[i][j]=triangle[i][j];
-                }else
-                {
-                    dp[i][j]=triangle[i][j]+min(dp[i+1][j],dp[i+1][j+1]);
-                }
-            }
+    // Memoization table (dp) initialized with -1 to indicate uncomputed
+    
+    vector<vector<int>> dp;
+
+    int solve(vector<vector<int>>& triangle, int i, int j) {
+        if (i == triangle.size()) {
+            return 0;
         }
-        return dp[0][0];
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int down = solve(triangle, i + 1, j);
+        int diagonal = solve(triangle, i + 1, j + 1);
+        return dp[i][j] = triangle[i][j] + min(down, diagonal);
+    }
+
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        dp.resize(n, vector<int>(n, -1));
+
+        return solve(triangle, 0, 0);
     }
 };
