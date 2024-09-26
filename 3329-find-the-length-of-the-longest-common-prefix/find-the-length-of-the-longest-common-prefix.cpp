c4f66d0e9d25f018ff0c543a511071fc758ba5cc
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-09-26 22:43:38 +0530

    Time: 243 ms (76.84%) | Memory: 160.9 MB (55.36%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -1,33 +1,95 @@
+struct TrieNode {
+    TrieNode* children[10]; // 0, 1, 2...9
+};
 
 class Solution {
 public:
+    TrieNode* getTrieNode() {
+        TrieNode* node = new TrieNode();
+        for (int i = 0; i < 10; i++) {
+            node->children[i] = nullptr;
+        }
+
+        return node;
+    }
+
+    void insert(int num, TrieNode* root) {
+        TrieNode* crawl = root;
+        string numStr = to_string(num);
+
+        for (char ch : numStr) {
+            int idx = ch - '0';
+            if (!crawl->children[idx]) {
+                crawl->children[idx] = getTrieNode();
+            }
+
+            crawl = crawl->children[idx];
+        }
+    }
+
+    // returns the length of th elongest prefix
+    int search(int num, TrieNode* root) {
+        TrieNode* crawl = root;
+        string numStr = to_string(num);
+        int length = 0;
+
+        for (char ch : numStr) {
+            int idx = ch - '0';
+            if (crawl->children[idx]) {
+                length++;
+                crawl = crawl->children[idx];
+            } else {
+                break;
+            }
+        }
+
+        return length;
+    }
+
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        int n = arr1.size();
-        int m = arr2.size();
-        int ans = 0;
+        TrieNode* root = getTrieNode(); // TRIE
 
-        set<string> s1;
-
-        for (int i = 0; i < n; i++) {
-            string numStr = to_string(arr1[i]);
-            for (int k = 1; k <= numStr.length(); k++) {
-                s1.insert(numStr.substr(0, k));
-            }
+        for (int num : arr1) {
+            insert(num, root);
         }
 
-        for (int i = 0; i < m; i++) {
-            string numStr = to_string(arr2[i]);
-            for (int k = 1; k <= numStr.length(); k++) {
-                string prefix = numStr.substr(0, k);
-                if (s1.find(prefix) != s1.end()) {
-                    ans = max(ans, (int)prefix.length());
-                }
-            }
+        int result = 0;
+        for (int num : arr2) {
+            result = max(result, search(num, root));
         }
 
-        return ans;
+        return result;
     }
 };
+// class Solution {
+// public:
+//     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+//         int n = arr1.size();
+//         int m = arr2.size();
+//         int ans = 0;
+
+//         set<string> s1;
+
+//         for (int i = 0; i < n; i++) {
+//             string numStr = to_string(arr1[i]);
+//             for (int k = 1; k <= numStr.length(); k++) {
+//                 s1.insert(numStr.substr(0, k));
+//             }
+//         }
+
+//         for (int i = 0; i < m; i++) {
+//             string numStr = to_string(arr2[i]);
+//             for (int k = 1; k <= numStr.length(); k++) {
+//                 string prefix = numStr.substr(0, k);
+//                 if (s1.find(prefix) != s1.end()) {
+//                     ans = max(ans, (int)prefix.length());
+//                 }
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
 
 // class Solution {
 // public:
