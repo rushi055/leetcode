b9258add76ca52400e133b534da66dc4723150e1
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-11-04 19:42:22 +0530

    Time: 28 ms (68.38%) | Memory: 29.8 MB (42.73%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.cpp
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -3,30 +3,17 @@ public:
     string compressedString(string word) {
         string ans = "";
         int n = word.length();
-        if (n == 0)
-            return ans;
+        int i = 0;
 
-        int k = 0;
-        char c = word[0];
-
-        for (int i = 0; i <= n; i++) {
-            if (k >= 9) {
-                ans += to_string(k);
-                ans.push_back(c);
-                k = 0;
-            }
-            if (i < n && word[i] == c) {
+        while (i < n) {
+            char c = word[i];
+            int k = 0;
+            while (i < n && word[i] == c && k < 9) {
                 k++;
-            } else {
-                if (k > 0) {
-                    ans += to_string(k);
-                    ans.push_back(c);
-                }
-                if (i < n) {
-                    c = word[i];
-                    k = 1;
-                }
+                i++;
             }
+            ans += to_string(k);
+            ans.push_back(c);
         }
 
         return ans;
