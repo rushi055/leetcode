36d2139a8f59eb0e6b248c56a51fdf2af12b66f1
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-11-10 15:46:52 +0530

    Time: 5 ms (20.49%) | Memory: 13.5 MB (5.54%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.cpp
+++ 3394-minimum-array-end/minimum-array-end.cpp
@@ -1,12 +1,39 @@
 class Solution {
 public:
-     long long minEnd(int n, int x) {
-        long long num = x;
-
-        for(int i = 1; i < n; i++) {
-            num = (num+1) | x;
+    std::string intToBinary64(int n) {
+        if (n == 0)
+            return std::string(64, '0');
+        std::string binary = "";
+        while (n > 0) {
+            binary = (n % 2 == 0 ? "0" : "1") + binary;
+            n /= 2;
         }
+        while (binary.size() < 64) {
+            binary = "0" + binary;
+        }
+        return binary;
+    }
+    std::string intToBinary(int n) {
+        std::string binary = "";
+        while (n > 0) {
+            binary = (n % 2 == 0 ? "0" : "1") + binary;
+            n /= 2;
+        }
+        return binary;
+    }
+    long long minEnd(int n, int x) {
+        std::string a = intToBinary(n - 1);//a=n-1
+        std::string k = intToBinary64(x);   
 
-        return num;
+        int j = a.length() - 1;
+        int i = k.length() - 1;
+        while (i >= 0 && j >= 0) {
+            if (k[i] != '1') {
+                k[i] = a[j];
+                j--;
+            }
+            i--;
+        }
+        return strtoull(k.c_str(), nullptr, 2);
     }
 };
